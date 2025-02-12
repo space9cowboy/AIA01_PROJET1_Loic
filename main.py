4364733738c3203d57260a97462c080806a7e3b2
Loic Rabearivony <lrabearivony@lemonway.com>
2025-02-12 10:49:53 +0100

    Add comment in files for documentation

--- main.py
+++ main.py
@@ -3,12 +3,21 @@ from src.data.data_processing import DataProcessing
 from config import DATA_PATH, RAW_DATA_PATH, PROCESSED_DATA_PATH
 import logging
 
+# ================================================================
+# CONFIGURATION DU LOGGING
+# ================================================================
+
 def setup_logging():
     """
     Configure le logging pour le projet
     """
     logging.basicConfig(level=logging.INFO)
     return logging.getLogger(__name__)
+
+
+# ================================================================
+# FONCTION PRINCIPALE
+# ================================================================
 
 def main():
     """
@@ -17,19 +26,33 @@ def main():
     # Configuration du logging
     setup_logging()
     logger = logging.getLogger(__name__)
-
     logger.info("Starting dataloading import")
-   
-    # loader = DataLoader(dataset_path= RAW_DATA_PATH, datasets=['category_tree'])
     data_processing = DataProcessing(data=['items_prop_1'])
+    
+    # Exécute le pipeline de prétraitement
     finaldf = data_processing.preprocess_data()
     
+    # ================================================================
+    # ANALYSE DES DONNÉES
+    # ================================================================
+    
     for dataset_name, dataset in finaldf.items():
-        print(f"Dataset: {dataset_name}")
-        print(dataset.head())
+        # print(f"Dataset: {dataset_name}")
+        # print(dataset.head())
+        # print(dataset.info())
+        # print(dataset.describe())
+        # print(dataset['itemid'].value_counts())
+        # print(dataset['timestamp'].value_counts())
+        # temp = dataset.groupby('itemid').agg({'itemid': 'size', 'timestamp': 'count'})
+        # temp = temp.rename(columns={'itemid': 'count_itemid', 'timestamp': 'count_timestamp'})
+        # temp = temp.reset_index()
+        # temp.sort_values(by='count_itemid', ascending=False, inplace=True)
+        dataunique = dataset[dataset['itemid'] == 158903]
+        print(dataunique.head(10))
+        # print(temp.head(10))
         print("\n")
     logger.info("Data loaded successfully")
-    # print(finaldf.head())
+
     logger.info("Starting preprocessing")
     
 if __name__ == "__main__":
--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -13,15 +13,25 @@ class DataLoader:
         Initialisation le chemin du dataset
         """
         self.dataset_path = dataset_path
+        
+        # Dictionnaire des datasets disponibles (nom logique → fichier CSV correspondant)
         self.available_datasets = datasets = {
             "category_tree": "category_tree.csv",
             "items_prop_1": "item_properties_part1.csv",
             "items_prop_2": "item_properties_part2.csv",
             "events": "events.csv",
         }
+        
+        # Si `datasets` est spécifié, charge uniquement ces fichiers, sinon charge tout
         self.datasets = datasets if datasets else list(self.available_datasets.keys())
+        
+        # Initialisation de l'API Kaggle et authentification
         self.api = KaggleApi()
         self.api.authenticate()
+    
+    # ================================================================
+    # MÉTHODE POUR TÉLÉCHARGER LES DONNÉES
+    # ================================================================
     
     def download_data(self):
         """
@@ -33,19 +43,23 @@ class DataLoader:
             unzip=True
         )
 
+    # ================================================================
+    # MÉTHODE POUR CHARGER LES DONNÉES EN DATAFRAMES
+    # ================================================================
+    
     def load_data(self):
         """
         Charge les données du dataset emcommerce-dataset
         """
         try:
-            loaded_data = {}
+            loaded_data = {} # Dictionnaire pour stocker les DataFrames
             files_missing = False
-            for dataset in self.datasets:
+            for dataset in self.datasets: # Parcours tous les datasets demandés
                 file_path = os.path.join(self.dataset_path, self.available_datasets[dataset])
-                if os.path.exists(file_path):
+                if os.path.exists(file_path): # Si le fichier csv existe on le charge en DF
                     loaded_data[dataset] = pd.read_csv(file_path)
                 else:
-                    files_missing = True
+                    files_missing = True # Si fichier manquant on recharge tout
                     print(f"File {file_path} does not exist")
             if files_missing:
                 self.download_data()
